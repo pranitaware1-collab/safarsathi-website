85c579664fa88ec85b86f76faf4f3f130663f643
Pranit <pranitaware1@gmail.com>
2026-05-09 14:24:16 +0530

    Fix email timeout issue in contact API

--- server.ts
+++ server.ts
@@ -28,6 +28,14 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
+});
+
 // -------------------------
 // SAFE MAIL FUNCTION
 // -------------------------
